97bc329249d7da51391b48559fde74d7cd8739b6
Arturo Martinez Hernandez <48699567+arturo1234567@users.noreply.github.com>
2019-03-29 11:11:57 -0600

    Add files via upload

---
+++ Documents/workspace/PRUEBAS2017/src/TOMAS.java
@@ -0,0 +1,22 @@
+
+
+/**
+ *
+ * @author Tesoem
+ */
+public class TOMAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      float area_triangulo = 0;
+int base = 5;
+int altura = 4;
+
+area_triangulo = (base*altura)/2;
+
+System.out.println(area_triangulo );
+    }
+    
+}
